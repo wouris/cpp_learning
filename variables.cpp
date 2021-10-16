ea3905986e39a91fff8be69a7f2898975531c6ac
wouris <wouris44@gmail.com>
2021-10-16 14:30:57 +0200

    Initial commit

---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string name = "Samuel";
+    int age;
+
+    age = 18;
+
+    cout << "My name is " << name << "." << endl;
+    cout << "I am " << age << " years old." << endl;
+    cout << "I like to code and learn in C++" << endl;
+
+    name = "wouris";
+    cout << "I'd like to be called " << name << endl;
+    
+    return 0;
+}
