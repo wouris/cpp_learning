18037f0f22b5865693f3c1455fb85239beaccf8c
wouris <wouris44@gmail.com>
2021-10-16 14:57:30 +0200

    dataTypes - learned

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -4,8 +4,25 @@
 using namespace std;
 
 int main() {
-    
-    cout << "Hello World!" << endl;
-    
+
+    // character type data types    
+        char grade = 'A';
+        string phrase = "Hello";
+
+    // number type data types - can be negative
+        int num = 18;
+
+        // difference between float and double is only in how accurate they are. 
+        // double data type is more accurate and therefore will it will be used more freqently
+        double weight = 73.6;
+
+        //true & false
+        bool isMale = true;
+
+    cout << "Hi" << endl;
+    cout << num << endl;
+    cout << 10.5 << endl;
+    cout << false  << " - False" << "  |  " << true << " - True" << endl;
+
     return 0;
 }
