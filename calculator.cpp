efe7d8785292b0661e28f44240098471a97f11ed
wouris <wouris44@gmail.com>
2021-11-01 08:16:20 -0700

    basicCalcualtor learned

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    double num1, num2;
+
+    cout << "Enter the 1st number" << endl;
+    cin >> num1;
+
+    cout << "Enter the 2st number" << endl;
+    cin >> num2;
+
+    cout << num1 + num2;
+    
+    return 0;
+}
